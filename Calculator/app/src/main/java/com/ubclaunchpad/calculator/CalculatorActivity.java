85d557dd0c482a448ad824a613db3b7f68c4b6e4
Nancy <nancyjiang10@gmail.com>
2017-09-07 21:06:48 -0700

    Update CalculatorActivity.java

--- Calculator/app/src/main/java/com/ubclaunchpad/calculator/CalculatorActivity.java
+++ Calculator/app/src/main/java/com/ubclaunchpad/calculator/CalculatorActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener{
@@ -23,32 +24,55 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnClic
      */
     @Override
     public void onClick(View v) {
+        Double ans;
+        Integer entry1 = R.id.firstInput;
+        Integer entry2 = R.id.secondInput;
+        TextView result = (TextView) findViewById(R.id.Result);
+
         switch (v.getId())
         {
             case R.id.operation_add:
             {
-                //TODO add function
+                ans = Double.valueOf(entry1 + entry2);
+                result.setText(String.valueOf(ans));
                 break;
             }
             case R.id.operation_sub:
             {
-                //TODO subtract function
+                ans = Double.valueOf(entry1 - entry2);
+                result.setText(String.valueOf(ans));
                 break;
             }
             case R.id.operation_multi:
             {
-                //TODO multiply function
+                ans = Double.valueOf(entry1)*entry2;
+                result.setText(String.valueOf(ans));
                 break;
             }
             case R.id.operation_div:
             {
-                //TODO divide function
+                ans = Double.valueOf(entry1)/entry2;
+                result.setText(String.valueOf(ans));
                 break;
             }
-            //TODO any extra implmentations (optional)
+                //did not have time to complete the extra operations
+            //sine
+            case R.id.operation_extra1:{
+                break;
+            }
+            //cosine
+            case R.id.operation_extra2:{
+                break;
+
+            }
+            //tangent
+            case R.id.operation_extra3:{
+                break;
+
+            }
             default:
             {
-                Toast.makeText(this, "Click not implmented yet", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Click not implemented yet", Toast.LENGTH_LONG).show();
                 Log.e(TAG, "View id: " + v.getId() + " click not implemented yet");
                 break;
             }
